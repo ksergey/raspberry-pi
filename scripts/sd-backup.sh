658135df1b0bb8d5c1de0723ce8328ea62670f5c
Sergey Kovalevich <inndie@gmail.com>
2020-08-22 00:17:56 +0300

    Improved description

--- scripts/sd-backup.sh
+++ scripts/sd-backup.sh
@@ -6,14 +6,19 @@
 #   sd-backup.sh [device]
 #
 # By default "/dev/mmcblk0" used as device.
+#
+# Restore image to sd-card:
+#   gunzip --stdout sd-card-backup-20200822-001618.img.gz | sudo dd bs=4M status=progress of=/dev/mmcblk0
+#
+# More details at https://www.raspberrypi.org/documentation/linux/filesystem/backup.md
 
 input="$1"
 output="sd-card-backup-$(date +%Y%m%d-%H%M%S).img.gz"
 
 [[ ! -z "${input}" ]] || input="/dev/mmcblk0"
 
-echo "input=${input}"
-echo "output=${output}"
+echo "input=\"${input}\""
+echo "output=\"${output}\""
 
 # Execute dd
 dd bs=4M if="${input}" status=progress | gzip > "${output}"
